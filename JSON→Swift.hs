191e33181a704f54d534c03d3deb22de17ceca42
Stan Serebryakov <i@cfr.pw>
2015-04-15 01:25:41 +0300

    stateful translator stub

--- JSON→Swift.hs
+++ JSON→Swift.hs
@@ -1,19 +1,34 @@
+{-# LANGUAGE UnicodeSyntax #-}
 -- $ brew install ghc cabal-install
--- $ cabal install json
+-- $ cabal install json base-unicode-symbols
 -- $ ghc --make JSON→Swift.hs
 -- $ ./JSON→Swift <file.json
 
 module Main where
 
+import Control.Monad.State
 import qualified Text.JSON (decode)
 import Text.JSON hiding (decode)
-import Control.Monad.State
+import Prelude.Unicode
+import Control.Monad.Unicode
 
-decode :: String -> JSValue
-decode = either error id . resultToEither . Text.JSON.decode
+jsSwiftURL = "https://gist.github.com/cfr/a7ce3793cdf8f17c6412#file-json-swift-hs"
 
-toSwift :: JSValue -> State JSValue String
-toSwift = return . show
+data TranslatorState = TranslatorState
+    { json  ∷ JSValue
+    , swift ∷ String
+    }
 
-main = interact $ \json -> evalState (toSwift $ decode json) JSNull
+toSwift ∷ TranslatorState → String
+toSwift = evalState $ translate
+ where
+  translate = get ≫= return ∘ swift
+
+main = interact $
+          toSwift
+        ∘ flip TranslatorState ("// Generated with " ⧺ jsSwiftURL ⧺ "\n")
+        ∘ decode
+
+decode ∷ String → JSValue
+decode = either error id ∘ resultToEither ∘ Text.JSON.decode
 
