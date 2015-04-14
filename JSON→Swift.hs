660b211e23e4f8a6040ac4df35d19c9628349a19
Stan Serebryakov <i@cfr.pw>
2015-04-15 02:29:52 +0300

    iterating and printing json

--- JSON→Swift.hs
+++ JSON→Swift.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE UnicodeSyntax, ScopedTypeVariables #-}
 -- $ brew install ghc cabal-install
 -- $ cabal install json base-unicode-symbols
 -- $ ghc --make JSON→Swift.hs
@@ -11,6 +11,8 @@ import qualified Text.JSON (decode)
 import Text.JSON hiding (decode)
 import Prelude.Unicode
 import Control.Monad.Unicode
+import Control.Arrow.Unicode
+import Data.List (intercalate)
 
 jsSwiftURL = "https://gist.github.com/cfr/a7ce3793cdf8f17c6412#file-json-swift-hs"
 
@@ -19,10 +21,23 @@ data TranslatorState = TranslatorState
     , swift ∷ String
     }
 
+type Pair = (String, JSValue)
+
 toSwift ∷ TranslatorState → String
 toSwift = evalState $ translate
  where
-  translate = get ≫= return ∘ swift
+  --translate = get ≫= return ∘ show ∘ swift
+  toS v = toSwift (TranslatorState v "")
+  translate = do
+    j ← get ≫= return ∘ json
+    case j of
+      JSObject jso → let os ∷ [Pair] = fromJSObject jso
+                         po (k, v) = k ⧺ ": {" ⧺ toS v ⧺ "}, "
+                     in return $ concatMap po os
+
+      JSArray a → let l = intercalate ", " (map toS a)
+                  in return ("[" ⧺ l ⧺ "]")
+      b → return (show b)
 
 main = interact $
           toSwift
