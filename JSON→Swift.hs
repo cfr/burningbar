c7bbb513c1c19072938d7ba8da7773073d42ea0e
Stan Serebryakov <i@cfr.pw>
2015-04-26 19:11:27 +0300

    identation, spec example

--- JSON→Swift.hs
+++ JSON→Swift.hs
@@ -4,7 +4,7 @@
 -- $ cabal install json base-unicode-symbols
 -- $ curl -O https://gist.github.com/cfr/a7ce3793cdf8f17c6412/raw/JSON→Swift.hs
 -- $ chmod +x JSON→Swift.hs
--- $ ./JSON→Swift.hs <file.json
+-- $ ./JSON→Swift.hs <spec.json
 
 module Main where
 
@@ -44,17 +44,19 @@ data Language = Language
 
 swift ∷ Language
 swift = Language tbd tbd fromType fromRecord etcSwift where
- tbd = const "TBD"
+ tbd = const "// TBD"
  fromRecord (Record name vars) = "struct " ⧺ name ⧺ " {\n" ⧺ concat decls ⧺ "}\n"
-  where decls = initDecl:(map varDecl vars)
-        initDecl = "init(json: JSON) {\n" ⧺ concatMap initVar vars ⧺ "}\n"
+  where decls = initDecl : (map varDecl vars)
+        initDecl = s 4 ⧺ "init(json: JSON) {\n" ⧺ concatMap initVar vars ⧺ s 4 ⧺ "}\n"
         initVar (Variable n (Optional t)) = initWithElem n ⧺ "? " ⧺ fromType t ⧺ "\n"
         initVar (Variable n t) = initWithElem n ⧺ "! " ⧺ fromType t ⧺ "\n"
-        initWithElem n = n ⧺ " = json[" ⧺ n ⧺ "] as"
-        varDecl (Variable n t) = "let " ⧺ n ⧺ ": " ⧺ fromType t ⧺ "\n"
+        initWithElem n = s 8 ⧺ n ⧺ " = json[" ⧺ n ⧺ "] as"
+        varDecl (Variable n t) = s 4 ⧺ "let " ⧺ n ⧺ ": " ⧺ fromType t ⧺ "\n"
+        s = concat ∘ flip take spaces
+        spaces = repeat " "
  fromType (Array t) = "[" ⧺ fromType t ⧺ "]"
  fromType (Optional t) = fromType t ⧺ "?"
- fromType (Dictionary tk tv) = "{ " ⧺ fromType tk ⧺ " : " ⧺ fromType tv ⧺ " }"
+ fromType (Dictionary tk tv) = "[" ⧺ fromType tk ⧺ " : " ⧺ fromType tv ⧺ "]"
  fromType (Typename typename) = typename
  etcSwift = "typealias JSON = Dictionary<String, String>\n"
 
