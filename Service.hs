5e428b189e1cd710d1ae7d5466d0663e6d9e11cd
Stan Serebryakov <i@cfr.pw>
2015-05-11 15:08:04 +0300

    burning bar as HTTP service

--- Service.hs
+++ Service.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE UnicodeSyntax, NamedFieldPuns #-}
+{-# LANGUAGE UnicodeSyntax #-}
 import Network.HTTP.Server
 import Network.HTTP.Server.Logger
 import Network.HTTP.Server.HtmlForm as Form
@@ -6,20 +6,25 @@ import Network.URL as URL
 import Codec.Binary.UTF8.String
 import Control.Exception (try, SomeException)
 import System.FilePath
+
 import Unicode
+import Swift
+import Parse
 
 main = serverWith config process
   where process _ url request = case rqMethod request of
-              POST → (return ∘ sendText OK ∘ decodeString ∘ rqBody) request
-              otherwise → return $ sendText BadRequest "Ｃ:。ミ"
+          POST → (return ∘ sendText OK ∘ decodeString ∘ rqBody) request
+          otherwise → return $ sendText BadRequest "Ｃ:。ミ"
 
 sendText ∷ StatusCode → String → Response String
-sendText s v = let txt = encodeString v in insertHeader HdrContentLength (show (length txt))
-             $ insertHeader HdrContentEncoding "UTF-8"
-             $ insertHeader HdrContentEncoding "text/plain"
-             $ (respond s ∷ Response String) { rspBody = txt }
+sendText s v = headers reponse
+  where reponse = (respond s ∷ Response String) { rspBody = toSwift text }
+        headers = insertHeader HdrContentLength (show $ length text)
+                ∘ insertHeader HdrContentEncoding "UTF-8"
+                ∘ insertHeader HdrContentType "text/plain"
+        text = encodeString v
 
-config = let srvLog = stdLogger
-             srvPort = 9604
-         in defaultConfig { srvLog, srvPort }
+toSwift = show ∘ translator (swift "Singularity" "Horizon") ∘ parse
+
+config = defaultConfig { srvLog = stdLogger, srvPort = 9604 }
 
